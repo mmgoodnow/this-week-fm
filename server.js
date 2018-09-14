53dfd5b9a34ca488e765654091925c592b17ed25
Michael Goodnow <mmgoodnow@gmail.com>
2018-09-14 18:53:46 -0400

    add HTTPS enforcement

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const enforce = require("express-sslify");
 const path = require("path");
 const app = express();
 
 // serve front end
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(express.static("./dist/this-week-fm"));
 
 app.get("/*", function(req, res) {
