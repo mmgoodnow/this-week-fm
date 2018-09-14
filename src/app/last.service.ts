f513b4a2c2c707dd7e073b2f3c3715cd39ba7ba5
Michael Goodnow <mmgoodnow@gmail.com>
2018-09-14 00:49:41 -0400

    barebones this week rankings

--- src/app/last.service.ts
+++ src/app/last.service.ts
@@ -17,9 +17,35 @@ export class LastService {
 		);
 	}
 
-	getThisWeekTracks(user) {
+	getThisWeekTracks(user, from: Date, to: Date) {
 		const request = {
 			method: "user.getrecenttracks",
+			user: user,
+			from: Math.floor(from.valueOf() / 1000),
+			to: Math.floor(to.valueOf() / 1000),
+			api_key: apikey,
+			format: "json",
+		};
+		return fetch(url + this.queryString(request)).then(response =>
+			response.json()
+		);
+	}
+
+	getFriends(user) {
+		const request = {
+			method: "user.getFriends",
+			user: user,
+			api_key: apikey,
+			format: "json",
+		};
+		return fetch(url + this.queryString(request)).then(response =>
+			response.json()
+		);
+	}
+
+	getInfo(user) {
+		const request = {
+			method: "user.getInfo",
 			user: user,
 			api_key: apikey,
 			format: "json",
--- src/app/week/week.component.ts
+++ src/app/week/week.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { LastService } from "../last.service";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
 	selector: "app-week",
@@ -7,11 +8,74 @@ import { LastService } from "../last.service";
 	styleUrls: ["./week.component.css"],
 })
 export class WeekComponent implements OnInit {
-	constructor(private service: LastService) {}
+	constructor(private service: LastService, private route: ActivatedRoute) {
+		this.setParams = this.setParams.bind(this);
+		this.route.params.subscribe(params => this.setParams(params));
+	}
 
-	ngOnInit() {
+	user: any;
+	friends: any[];
+
+	ngOnInit() {}
+
+	private setParams(params) {
+		this.user = params.userId;
+		this.loadUsers();
+	}
+
+	private getLastFriday(): Date {
+		const now = new Date();
+		const diff = (7 - 5 + now.getDay()) % 7;
+		const lastFriday = new Date();
+		lastFriday.setDate(now.getDate() - diff);
+		lastFriday.setHours(0);
+		lastFriday.setMinutes(0);
+		lastFriday.setSeconds(0);
+		lastFriday.setMilliseconds(0);
+		lastFriday.setDate(-7);
+		console.log(lastFriday);
+		return lastFriday;
+	}
+
+	public get sortedFriends(): any[] {
+		this.friends.sort((a, b) => b.thisWeekTracks - a.thisWeekTracks);
+		return this.friends;
+	}
+
+	private loadUsers() {
+		this.friends = [];
+
 		this.service
-			.getThisWeekTracks("mmgoodnow")
-			.then(tracks => console.log(tracks));
+			.getInfo(this.user)
+			.then(res => {
+				console.log(res);
+				this.user = res.user;
+				this.friends.push(res.user);
+			})
+			.then(() =>
+				this.service
+					.getFriends(this.user.name)
+					.then(res => {
+						console.log(res);
+						this.friends.push(...res.friends.user);
+					})
+					.then(() => {
+						for (let i = 0; i < this.friends.length; i++) {
+							const arr = this.friends;
+							const user = arr[i];
+							this.service
+								.getThisWeekTracks(
+									user.name,
+									this.getLastFriday(),
+									new Date()
+								)
+								.then(res => {
+									user.thisWeekTracks = Number(
+										res.recenttracks["@attr"].total
+									);
+								});
+						}
+					})
+			);
 	}
 }
