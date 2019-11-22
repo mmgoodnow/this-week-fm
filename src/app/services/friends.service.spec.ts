d3652a73e81e5d244847b91bc173263b5f265a3a
Michael Goodnow <mmgoodnow@gmail.com>
2019-11-22 01:37:51 -0500

    Do prettier

--- src/app/services/friends.service.spec.ts
+++ src/app/services/friends.service.spec.ts
@@ -5,7 +5,8 @@ import { HttpClient } from "@angular/common/http";
 
 describe("FriendsService", () => {
 	beforeEach(() =>
-		TestBed.configureTestingModule({ declarations: [HttpClient] }));
+		TestBed.configureTestingModule({ declarations: [HttpClient] })
+	);
 
 	it("should be created", () => {
 		const service: FriendsService = TestBed.get(FriendsService);
